4270a0d54378fd9269bb3898aecc2e28983b50ec
skyran1278 <skyran1278@gmail.com>
2021-01-19 09:25:20 +0800

    錯誤紀錄 and 大量測試

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const { people, bookCourts } = require('./config');
 const userAgent =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
 
-const loginJob = new CronJob('50 59 23 * * 4', () => {
+const loginJob = new CronJob('50 59 23 * * *', () => {
   Object.entries(people).forEach(([name, person]) => {
     const formData = new FormData();
     formData.append('account', person.account);
@@ -34,9 +34,9 @@ const loginJob = new CronJob('50 59 23 * * 4', () => {
   });
 });
 
-const bookBadmintonCourtJob = new CronJob('00 00 00 * * 5', () => {
-  const bookDate = format(add(new Date(), { days: 7 }), 'yyyy-MM-dd');
+const bookDate = format(add(new Date(), { days: 7 }), 'yyyy-MM-dd');
 
+const bookBadmintonCourt = () => {
   bookCourts.forEach(({ court, time, person }) => {
     fetch(
       `http://nd01.allec.com.tw/MobilePlace/MobilePlace?tFlag=3&PlaceType=1&BookingPlaceID=${court}&BookingDate=${bookDate}&BookingTime=${time}`,
@@ -49,17 +49,29 @@ const bookBadmintonCourtJob = new CronJob('00 00 00 * * 5', () => {
       }
     )
       .then((res) => res.text())
-      .then((body) =>
+      .then((body) => {
+        const bookingTime = format(new Date(), 'yyyy-MM-dd-HH-mm-ss-SSS');
         console.log(
           /預約成功/.test(body)
-            ? `${court} ${time} 預約成功`
-            : `${court} ${time} 預約失敗\n${body}`
-        )
-      );
+            ? `${court} ${time} 預約成功 ${bookingTime}`
+            : `${court} ${time} 預約失敗 ${bookingTime}\n${body}`
+        );
+      });
   });
-});
+};
 
-bookBadmintonCourtJob.start();
+const bookBadmintonCourt00Job = new CronJob(
+  '00 00 00 * * *',
+  bookBadmintonCourt
+);
+
+const bookBadmintonCourt01Job = new CronJob(
+  '01 00 00 * * *',
+  bookBadmintonCourt
+);
+
 loginJob.start();
+bookBadmintonCourt00Job.start();
+bookBadmintonCourt01Job.start();
 
 console.log('cron started');
